919b37bd840730ccf8a478d39463cfb27c5d8666
Espen Haugen <espenhaugen14@gmail.com>
2024-08-29 04:39:27 +0000

    Oppdatert en del ting

--- models/staging/STG_ABC_BANK_SECURITY_INFO.sql
+++ models/staging/STG_ABC_BANK_SECURITY_INFO.sql
@@ -15,8 +15,8 @@ with
  ),
  hashed as (
     SELECT 
-        concat_ws('|',SECURITY_CODE) as SECURITY_HKEY
-       ,concat_ws('|',SECURITY_CODE,SECTOR_NAME,INDUSTRY_NAME,COUNTRY_CODE,EXCHANGE_CODE) AS SECURITY_HDIFF
+        {{dbt_utils.surrogate_key(['SECURITY_CODE'])}} as SECURITY_HKEY 
+       ,{{dbt_utils.surrogate_key(["SECURITY_CODE","SECTOR_NAME","INDUSTRY_NAME","COUNTRY_CODE","EXCHANGE_CODE"])}} AS SECURITY_HDIFF
        ,* EXCLUDE LOAD_TS
        ,LOAD_TS as LOAD_TS_UTC
     FROM src_data
